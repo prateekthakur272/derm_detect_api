2199e5bc23b05fff920c8175b851f069ef3afbf3
Prateek Thakur <prateekthakur272@gmail.com>
2023-12-18 22:58:06 +0530

    adds redister function

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+# Prateek Thakur 2023
+
+# import packages
 from fastapi import FastAPI
 import uvicorn
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, auth
 import pyrebase
 from firebase_config import firebaseConfig
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
+
+# import models
+from models.login_schema import LoginSchema
+from models.register_schema import RegisterSchema
+
+
 description = 'API for Derm-Detect cancer detection application'
 title = 'Derm-Detect'
 api_route = '/api'
@@ -27,15 +38,25 @@ def index():
     }
     
 @app.post(f'{api_route}/auth/login')
-def login():
+def create_access_token(user_credentials:LoginSchema):
     pass
 
 @app.post(f'{api_route}/auth/register')
-def register():
-    pass
+def create_account(user_credentials:RegisterSchema):
+    email = user_credentials.email
+    password = user_credentials.password
+    
+    try:
+        user = auth.create_user(email=email, password=password)
+        return JSONResponse(content={
+        'message': f'account created successfuly for {email} uid: {user.uid}',
+        },status_code=201)
+    except auth.EmailAlreadyExistsError:
+        raise HTTPException(status_code=400, detail= f'account already exist with email {email}')
+    
 
 @app.post(f'{api_route}/auth/ping')
-def ping():
+def validate_token():
     pass
 
 
